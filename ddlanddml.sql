b3c340f331e8809af1828e20d27be80f5309ed8f
iamthego0ke <40718785+iamthego0ke@users.noreply.github.com>
2026-02-21 14:29:42 +0100

    Delete ddlanddml.sql

--- ddlanddml.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE customers (
-    id NUMBER PRIMARY KEY generated always as identity(1,1),
-    name VARCHAR2(100),
-    created DATE DEFAULT SYSDATE
-);
-INSERT INTO customers (id, name) VALUES (1, 'Test User');
-COMMIT;
